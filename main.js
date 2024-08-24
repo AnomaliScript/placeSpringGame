0ddf78e1bca07fdc6f70fe11f318f0d4062ba739
AnomaliScript <anomaliscript@gmail.com>
2024-08-24 10:52:37 -0400

    Making pseudo-gravity (part the third)

--- main.js
+++ main.js
@@ -8,12 +8,25 @@ https://sprig.hackclub.com/gallery/getting_started
 @addedOn: 2024-08-10
 */
 
+// Static stuff
 const floor = 16;
 const player = "p";
-const spike = "b";
+const spike = "s";
+const block = "b";
+const glass = "g";
+const glassBroken = "r";
+const crate = "c";
+const door = "d";
+
+// Physics
 var airborne = true;
 var jumpRefresh = 0;
+const jumpHeight = 3;
 
+// Upside-down B)
+const gravitySwitch = "w";
+var reverseGravity = false;
+const UDplayer = "u";
 
 setLegend(
   [player, bitmap`
@@ -33,6 +46,7 @@ setLegend(
 ................
 ................
 ................`],
+  [],
   [spike, bitmap`
 ................
 ................
@@ -50,10 +64,97 @@ setLegend(
 .LLLLLLLLLLLLLL.
 .LLLLLLLLLLLLLL.
 LLLLLLLLLLLLLLLL`],
+  [block, bitmap`
+0000000000000000
+0000000010000000
+0000000001001000
+0000000000100100
+0000000000010010
+0000000000001000
+0000000000000100
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000`],
+  [glass, bitmap`
+7777777777777777
+7222222222222227
+7222222222722227
+7222222222272227
+7222222222227227
+7222222222222727
+7222222222222227
+7222222222222227
+7222222222222227
+7222222222222227
+7222222222222227
+7222222222222227
+7222222222222227
+7222222222222227
+7222222222222227
+7777777777777777`],
+  [glassBroken, bitmap`
+7777777...77777.
+7......77..77.7.
+7........7..7.77
+7........7..77.7
+7........77..7.7
+77....777.7..7.7
+77....7..77..7.7
+777..7......7..7
+..777..77...7..7
+......7.7..7...7
+...777..7.77..7.
+.777....7.7...77
+77......7.77...7
+.77.....7..777.7
+..7......7....77
+..777777777.....`],
+  [crate, bitmap`
+1111111111111111
+1CC1C1CCC1C1CCC1
+1CCC11CCC1C1CCC1
+11CCC1CCC1C1CCC1
+1C1CCC1CC1C1CCC1
+1CC1CCC1C1C1CCC1
+1CCC1CCC11C1CCC1
+1CCC11CCC1C1CCC1
+1CCC1C1CCC11CCC1
+1CCC1C11CCC1CCC1
+1CCC1C1C1CCC1CC1
+1CCC1C1CC1CCC1C1
+1CCC1C1CCC1CCC11
+1CCC1C1CCC11CCC1
+1CCC1C1CCC1C1CC1
+1111111111111111`],
+  [gravitySwitch, ],
+  [door, bitmap`
+................
+................
+..........CCC...
+.....CCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCCC..
+..CCCCCCCCCCCC..
+...CCCCCCCCCCC..
+...CCCCCCC66CC..
+...CCCCCCC66CC..
+...CCCCCCCCCCC..
+...CCCCCCCCCCC..
+...CCCCCCCCCCCC.
+....CCCCCCCCCCC.
+....CCCCCCCCCCC.
+....CCCCCCCCCCC.`]
 )
 
 // Solids
-setSolids([])
+setSolids([player, block, glass])
 
 let level = 0
 const levels = [{
@@ -74,9 +175,8 @@ const levels = [{
 .................
 .................
 .................
-.................`,
+.........ss......`,
   playerPos: { x: 5, y: floor},
-  spikePos: { x: 9, y: floor},
 }]
 
 let currentLevel = 0;
@@ -87,7 +187,6 @@ const drawLevel = (levelIndex) => {
 
   // Add player and spike sprites
   addSprite(level.playerPos.x, level.playerPos.y, player);
-  addSprite(level.spikePos.x, level.spikePos.y, spike);
 };
 drawLevel(currentLevel);
 
@@ -98,40 +197,49 @@ setPushables({
 
 // Keybinds
 onInput("w", () => {
-  getFirst(player).y -= 1;
+  if (!airborne) {
+    getFirst(player).y -= jumpHeight
+    airborne = true;
+    if (jumpRefresh == 0) {
+      jumpRefresh = jumpHeight;
+    }
+  }
 });
 
 onInput("a", () => {
   getFirst(player).x -= 1;
 });
 
-/* onInput("s", () => {
-  getFirst(player).y += 1;
-}); */
-
 onInput("d", () => {
   getFirst(player).x += 1;
 });
 
-
+// AFTERINPUT RAHHHH
 afterInput(() => {
+  // Gravity (will happen every time when a new button is pressed because that's the only way time can go forward)
+  if (airborne && jumpRefresh != 0) {
+    getFirst(player).y += 1;
+    jumpRefresh -= 1;
+  }
+  if (jumpRefresh == 0) {
+    airborne = false;
+  }
+  
   // Coords
   let playerPosition = getFirst(player); // Get the spike sprite
-  let spikePosition = getFirst(spike); // Get the spike sprite
+  let spikePosition = getAll(spike); // Get the spike sprite
   if (playerPosition) {
     const playerCoords = `${playerPosition.x}, ${playerPosition.y}`; // Get the x and y coordinates of the player
     clearText(); // Clear previous text on the screen
     addText(playerCoords, { x: 8, y: 1, color: color`5`, scale: 0.8 }); // Display the player coordinates
   }
 
-  // "Text when on top of something" concept
-  if (playerPosition.x == spikePosition.x && playerPosition.y == spikePosition.y) {
-    addText("you died :(", { x: 3, y: 3, scale: 0.5, color: color`0`});
-  }
+  // Spike death check
+  let spikes = getAll(spike);
 
-  // Gravity (will happen every time when a new key is pressed because that's th only way time can go forward)
-  if (airborne) {
-    // Time only "happens" when you press the next key
-    setInterval(playerY += 1, 1000);
+  for (let i = 0; i < spikes.length; i++) {
+    if (playerPosition.x == spikes[i].x && playerPosition.y == spikes[i].y) {
+      addText("you died :(", { x: 3, y: 3, scale: 0.5, color: color`0`});
+    }
   }
 })
